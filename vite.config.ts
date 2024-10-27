31d4b3eec3439dfe096b8b8ea496069a2d4ecc21
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-10-27 13:12:03 +0800

    Vite Configurations

--- vite.config.ts
+++ vite.config.ts
@@ -15,10 +15,5 @@ export default defineConfig(({ command }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    define: {
-      global: {
-        basename: isProd ? basenameProd : "",
-      },
-    },
   }
 })
